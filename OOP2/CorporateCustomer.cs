df4a8885280a074a2fc5a1d82d814c54a9110936
SevgiKocak <sevgi.kocak.09@gmail.com>
2021-01-24 20:36:23 +0300

    Five.

--- OOP2/CorporateCustomer.cs
+++ OOP2/CorporateCustomer.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace OOP2
 {
-    class CorporateCustomer: Customer  // Customer sınıfı ebeveyndir yani base class türeten sınıf.
-                                      //  CopporateCustomer sınıfıda çocuktur diyebiliriz yani derived class türeyen sınıf.
+    // Customer sınıfı ebeveyndir yani base class türeten sınıf.
+    //  CopporateCustomer sınıfıda çocuktur diyebiliriz yani derived class türeyen sınıf.
+    class CorporateCustomer : Customer 
     {
         //Tüzel Müşteri
        
